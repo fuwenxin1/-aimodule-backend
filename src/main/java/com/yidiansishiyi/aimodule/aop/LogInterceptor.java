19eb18103bb9781be7b41fc0f02fc4da524f2326
threeSeven <nuomibaicha@163.com>
2023-07-28 17:57:08 +0800

    update 无关业务清除

--- src/main/java/com/yidiansishiyi/aimodule/aop/LogInterceptor.java
+++ src/main/java/com/yidiansishiyi/aimodule/aop/LogInterceptor.java
@@ -18,7 +18,6 @@ import java.util.UUID;
  * 请求响应日志 AOP
  *
  * @author sanqi
- *   
  **/
 @Aspect
 @Component
@@ -28,7 +27,7 @@ public class LogInterceptor {
     /**
      * 执行拦截
      */
-    @Around("execution(* com.yupi.springbootinit.controller.*.*(..))")
+    @Around("execution(* com.yidiansishiyi.aimodule.controller.*.*(..))")
     public Object doInterceptor(ProceedingJoinPoint point) throws Throwable {
         // 计时
         StopWatch stopWatch = new StopWatch();
--- src/main/java/com/yidiansishiyi/aimodule/mapper/UserMapper.java
+++ src/main/java/com/yidiansishiyi/aimodule/mapper/UserMapper.java
@@ -1,10 +1,10 @@
 package com.yidiansishiyi.aimodule.mapper;
 
-import com.yidiansishiyi.aimodule.model.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yidiansishiyi.aimodule.model.entity.User;
 
 /**
- * @Entity com.yupi.springbootinit.model.entity.User
+ * @Entity com.yidiansishiyi.aimodule.model.entity.User
  */
 public interface UserMapper extends BaseMapper<User> {
 
